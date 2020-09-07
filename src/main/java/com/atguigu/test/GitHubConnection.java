5bd5ce21338369f70ae0f8d6605367ebcf4030c3
xialei <2513202406@qq.com>
2020-09-07 16:35:32 +0800

    第一次更改

--- src/main/java/com/atguigu/test/GitHubConnection.java
+++ src/main/java/com/atguigu/test/GitHubConnection.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class GitHubConnection {
     public static void main(String[] args) {
         System.out.println("xialei");
+        System.out.println("第一次更改");
     }
 }
